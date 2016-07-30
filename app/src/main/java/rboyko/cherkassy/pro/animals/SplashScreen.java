e5e2ff70b73842bafec5869baa04cf339530a832
rboykock <rboykock@gmail.com>
2016-07-30 23:34:50 +0300

    Подключил кастомный шрифт в splashscreen и добавил для различных языков название программы в ресурсы

--- app/src/main/java/rboyko/cherkassy/pro/animals/SplashScreen.java
+++ app/src/main/java/rboyko/cherkassy/pro/animals/SplashScreen.java
@@ -1,9 +1,11 @@
 package rboyko.cherkassy.pro.animals;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -12,6 +14,10 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        TextView app_title_view=(TextView) findViewById(R.id.app_title);
+        Typeface font=Typeface.createFromAsset(getAssets(),"fonts/9974.ttf");
+        app_title_view.setTypeface(font);
+
         Thread trThread=new Thread() {
             public void run() {
                 try {
